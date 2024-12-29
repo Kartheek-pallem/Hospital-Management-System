1862ea596985b438e57985e3cc80e965bca70f4e
kartheek <kartheekpallem@gmail.com>
2024-12-29 14:21:47 +0530

    CSS Adjustments

--- src/main/resources/frontend/HMSApp/src/app/app.module.ts
+++ src/main/resources/frontend/HMSApp/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PatientComponent } from './patient/patient.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminDashboardComponent,
-    PatientComponent
+    PatientComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
