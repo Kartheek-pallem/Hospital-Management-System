651f1bad8b244bffb118617a2738e908abec44a9
kartheek <kartheekpallem@gmail.com>
2024-12-30 19:35:43 +0530

    Added appointment service ts file

---
+++ src/main/resources/frontend/HMSApp/src/app/services/appointment.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Appointment } from "../appointment/appointment.component";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AppointmentService{
+    constructor(private httpClient:HttpClient){
+
+    }
+
+    private baseURL = "http://localhost:8080/appointments/get";
+    getAppointmentsList(){
+        return this.httpClient.get<Appointment[]>(this.baseURL);
+    }
+
+}
