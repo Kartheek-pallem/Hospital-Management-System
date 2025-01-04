e614960eeadf4455c363b7dfe7b35f6da11552a7
kartheek <kartheekpallem@gmail.com>
2025-01-04 13:18:27 +0530

    Added view appointment functionality

--- src/main/resources/frontend/HMSApp/src/app/appointment/appointment.component.ts
+++ src/main/resources/frontend/HMSApp/src/app/appointment/appointment.component.ts
@@ -4,6 +4,7 @@ import { AppointmentService } from '../services/appointment.service';
 import { AddAppointmentComponent } from '../add.appointment/add.appointment.component';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
+
 @Component({
   selector: 'app-appointment',
   standalone: false,
@@ -16,6 +17,19 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       state('hovered', style({ transform: 'scale(1.1)' })),
       transition('normal <=> hovered', animate('0.2s ease-in-out')),
     ]),
+    trigger('overlayAnimation', [
+      state('hidden', style({
+        opacity: 0,
+        transform: 'translateY(-50%)'
+      })),
+      state('visible', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      transition('hidden <=> visible', [
+        animate('0.3s ease-in-out')
+      ])
+    ])
   ]
 })
 
@@ -23,6 +37,8 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   providedIn: 'root', // Makes the service available application-wide
 })
 export class Appointment {
+  isOverlayVisible: boolean = false;
+  selectedAppointment:any;
 
   constructor(private appointmentService:AppointmentService,private dialog: MatDialog){
     
@@ -71,5 +87,27 @@ export class Appointment {
   onLeaveIcon() {
     this.hoverState = 'normal';
   }
+
+  showAppointment(appointment:any){
+    this.isOverlayVisible= !this.isOverlayVisible;
+    this.selectedAppointment=appointment;
+  }
+
+  closeOverlay(){
+    this.isOverlayVisible= !this.isOverlayVisible;
+  }
+
+  getAppointmentDetails(appointment: any): { label: string; value: any }[] {
+    return Object.entries(appointment).map(([key, value]) => ({
+        label: this.formatKey(key),
+        value,
+    }));
+  }
+
+  formatKey(key: string): string {
+      // Convert camelCase or snake_case to readable text
+      return key.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ').toUpperCase();
+  }
+
 
 }
